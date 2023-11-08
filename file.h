2d1cfb1f1c90fe4cbd2eec4da79484f8727dd670
Mitja Felicijan <mitja.felicijan@gmail.com>
2023-11-08 23:53:20 +0100

    Basic reading of files

---
+++ file.h
@@ -0,0 +1,13 @@
+#ifndef FILE_H
+#define FILE_H
+
+#include <stdio.h>
+
+struct FileContent {
+  const char *content;
+  size_t count;
+};
+
+struct FileContent read_entire_file(const char *file_path);
+
+#endif
