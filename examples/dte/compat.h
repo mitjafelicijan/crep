dcacc00e3750300617ba6e16eb346713f91a783a
Mitja Felicijan <mitja.felicijan@gmail.com>
2026-01-21 22:52:54 +0100

    Remove testing data

--- examples/dte/compat.h
+++
@@ -1,8 +0,0 @@
-#ifndef COMPAT_H
-#define COMPAT_H
-
-#include "../build/feature.h"
-
-extern const char feature_string[];
-
-#endif
--- examples/dte/shift.h
+++
@@ -1,8 +0,0 @@
-#ifndef SHIFT_H
-#define SHIFT_H
-
-#include "view.h"
-
-void shift_lines(View *view, int count);
-
-#endif
